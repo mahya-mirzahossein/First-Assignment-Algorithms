7183c5a090a6ecfdaae89b0f3da575eb6c1e6273
mahya m <mahya.m8844@gmail.com>
2025-04-10 23:16:42 +0330

    ch_1

--- AP1403 - Algorithms/src/main/java/Exercises.java
+++ AP1403 - Algorithms/src/main/java/Exercises.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Exercises {
 
     /*
@@ -17,7 +19,7 @@ public class Exercises {
                     if (target == (values[i] * values[j])){
                         result[0] = i;
                         result[1] = j;
-                        System.out.println(i + " " + j);
+                        //System.out.println(i + " " + j);
                         return result;
 
                     }
@@ -39,8 +41,42 @@ public class Exercises {
         so you should walk in that matrix in a curl and then add the numbers in order you've seen them in a 1D array
     */
     public int[] spiralTraversal(int[][] values, int rows, int cols) {
-        // todo
-        return null;
+        if (rows <= 0 || cols <= 0){
+            return null;
+        }
+        int k = 0;
+        int count = 0;
+        int i = 0;
+        int j = 0;
+        int[] result = new int[rows * cols];
+        for (; count < rows * cols;){
+            for(i = k; j < cols - k && count < rows * cols; j++){
+                result[count] = values[i][j];
+                //System.out.println(result[count]);
+                count++;
+            }
+            i++;
+            for (j = cols - k - 1;i < rows - k && count < rows * cols; i++){
+                result[count] = values[i][j];
+                //System.out.println(result[count]);
+                count++;
+            }
+            i--;
+            for (j = cols - k - 2; j >= k && count < rows * cols; j--){
+                result[count] = values[i][j];
+                //System.out.println(result[count]);
+                count++;
+            }
+            i--;
+            for(j = k ; i > k + 1 && count < rows * cols; i--){
+                result[count] = values[i][j];
+                //System.out.println(result[count]);
+                count++;
+            }
+            k++;
+        }
+        return result;
+
     }
 
     /*
@@ -67,15 +103,78 @@ public class Exercises {
 
         if you're familiar with lists and arraylists, you can also edit method's body to use them instead of array
     */
-    public int[][] intPartitions(int n) {
-        // todo
+    public ArrayList<Integer> intPartitions(int n) {
+        ArrayList<ArrayList<Integer>> result = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            result.add(new ArrayList<>());
+        }
+
+        int count = 0;
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        int sum = 0;
+        for (; k < n; k++){
+            if (count == n){
+                for (i = 0; i < n; i++) {
+                    result.add(new ArrayList<>());
+                }
+            }
+            for (i = 0; i < n / (n - k); i++){
+                result.get(count).add(n - k);
+                count++;
+            }
+            if (k != 0) {
+                result.get(count).add(k);
+            }
+            System.out.println(result.get(count));
+        }
         return null;
     }
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        //Exercises ex = new Exercises();
-        //int[] array = {5, 10, 2, 20};
-        //System.out.println(ex.productIndices(array, 20));
+        Exercises ex = new Exercises();
+        /*
+        int[] array = {5, 10, 2, 20};
+        System.out.println(ex.productIndices(array, 20));
+        int[] array2 = {3, 7, 1, 14};
+        System.out.println(ex.productIndices(array2, 21));
+        int[] array3 = {69, 15, 17, 23, 5, 11, 4, 6, 85, 39, 81, 34, 76, 21, 3, 36, 98, 77, 9, 42};
+        System.out.println(ex.productIndices(array3, 2898));
+
+        int[][] nums = {
+                {1, 2, 3, 4, 5}
+        };
+        int rows = 1;
+        int cols = 5;
+        System.out.print(ex.spiralTraversal(nums, rows, cols));
+        int[][] nums2 = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9},
+                {10, 11, 12}
+        };
+        int rows2 = 4;
+        int cols2 = 3;
+        System.out.print(ex.spiralTraversal(nums2, rows2, cols2));
+        int[][] nums3 = {
+                {1, 2, 3, 4},
+                {5, 6, 7, 8}
+        };
+        int rows3 = 2;
+        int cols3 = 4;
+        System.out.print(ex.spiralTraversal(nums3, rows3, cols3));
+        int[][] nums4 = {
+                {1, 2, 3, 4, 5, 6},
+                {7, 8, 9, 10, 11, 12},
+                {13, 14, 15, 16, 17, 18},
+                {19, 20, 21, 22, 23, 24},
+                {25, 26, 27, 28, 29, 30}
+        };
+        int rows4 = 5;
+        int cols4 = 6;
+        System.out.print(ex.spiralTraversal(nums4, rows4, cols4));
+         */
+        System.out.println(ex.intPartitions(6));
     }
 }
